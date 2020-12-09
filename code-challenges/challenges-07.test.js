a3ecf97becaca76feb2199e817826647ea35b0e4
nickmagruder <nick.magruder@outlook.com>
2020-12-08 21:51:42 -0800

    1 of 4 complete, WIP

--- code-challenges/challenges-07.test.js
+++ code-challenges/challenges-07.test.js
@@ -33,11 +33,11 @@ You may choose to complete this challenge using a for loop, for...in syntax, or 
 For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and 2 ^ 3 = 8.
 ------------------------------------------------------------------------------------------------ */
 
-const forLoopTwoToThe = (arr) => {
+/* const forLoopTwoToThe = (arr) => {
 arr.map((n, i) {
     return n * 2;
   }));
-}
+} */
 
 
 
